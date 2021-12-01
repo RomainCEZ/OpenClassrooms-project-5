8bcd729c5f122d3e8dc7564012079b562bae72dd
Romain <hercez92@hotmail.fr>
2021-12-01 21:17:35 +0100

    refactor: add class, change functions and variables names

--- front/js/script.js
+++ front/js/script.js
@@ -1,23 +1,41 @@
+class product {
+    constructor ({ _id, imageUrl, altTxt, name, description }) {
+        this.id = _id,
+        this.src = imageUrl,
+        this.alt = altTxt,
+        this.name = name,
+        this.description = description
+    }
 
-//gets products from api
-async function fetchProducts() {
-    let response = await fetch("http://localhost:3000/api/products");
-    products = await response.json();
-    console.table(products);
-}
-
-//parse products and add them to the products page
-function setProducts() {
-    const productsList = document.getElementById("items");
-    for (let i in products) {
-        const product = `<a href="./product.html?id=${products[i]._id}"><article><img src=${products[i].imageUrl} alt=${products[i].altTxt}/><h3 class="productName">${products[i].name}</h3><p class="productDescription">${products[i].description}</p></article></a>`;
-        productsList.innerHTML += product;
+    get insertDataIntoProductHtml() {
+        const productHtml = `
+            <a href="./product.html?id=${this.id}">
+                <article><img src=${this.src} alt=${this.alt}/>
+                    <h3 class="productName">${this.name}</h3>
+                    <p class="productDescription">${this.description}</p>
+                </article>
+            </a>`;
+        return productHtml;
     }
 }
 
+async function getProductsFromApi() {
+    const response = await fetch("http://localhost:3000/api/products");
+    products = await response.json();
+    return products;
+}
+
+function insertProductsIntoHtml() {
+    const productsList = document.getElementById("items");
+    products.forEach( products => {
+        const productHtml = new product(products).insertDataIntoProductHtml;
+        productsList.innerHTML += productHtml;
+    })
+}
+
 async function fillProductsPage() {
-    let products = await fetchProducts();
-    setProducts();
+    const products = await getProductsFromApi();
+    insertProductsIntoHtml();
 }
 
 fillProductsPage();
