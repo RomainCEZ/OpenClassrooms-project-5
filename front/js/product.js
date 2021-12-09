4735d579eb78860ba3b28362aa69400728cc56e4
Romain <hercez92@hotmail.fr>
2021-12-09 20:08:13 +0100

    refactor: change code structure from functions to classes

--- front/js/product.js
+++ front/js/product.js
@@ -1,71 +1,136 @@
 class Product {
-    constructor ({ _id, imageUrl, altTxt, name, description, colors, price }) {
-        this.id = _id,
-        this.src = imageUrl,
-        this.alt = altTxt,
+    constructor ({ id, img, name, description, colors, price }) {
+        this.id = id,
+        this.img = img,
         this.name = name,
         this.description = description,
         this.colors = colors,
         this.price = price
     }
-
-    get addProductImgToHtml() {
-        document.querySelector(".item__img")
-        .innerHTML = `<img src="${this.src}" alt="${this.alt}">`;
+    static createProduct(product) {
+        return new Product(product);
     }
+}
 
-    get addProductNameToHtml() {
+class CartProduct {
+    constructor ({ qty, color, product}) {
+        this.qty = qty,
+        this.color = color,
+        this.product = product
+    }
+    static createCartProduct({ qty, color, product}) {
+        return new CartProduct({ qty, color, product});
+    }
+}
+
+class ProductHtmlService {
+    static addProductNameToHtml(productName) {
         document.getElementById("title")
-            .innerHTML = `${this.name}`;
+            .innerHTML = `${productName}`;
     }
 
-    get addProductDescriptionToHtml() {
+    static addProductImgToHtml({productImgSrc, productImgAlt}) {
+        document.querySelector(".item__img")
+        .innerHTML = `<img src="${productImgSrc}" alt="${productImgAlt}">`;
+    }
+
+    static addProductDescriptionToHtml(productDescription) {
         document.getElementById("description")
-            .innerHTML = `${this.description}`;
+            .innerHTML = `${productDescription}`;
     }
 
-    get addProductPriceToHtml() {
+    static addProductPriceToHtml(productPrice) {
         document.getElementById("price")
-            .innerHTML = `${this.price}`;
+            .innerHTML = `${productPrice}`;
     }
 
-    get addProductColorsToHtml() {
-        this.colors.forEach( color => {
+    static addProductColorsToHtml(productColors) {
+        productColors.forEach( color => {
             document.getElementById("colors")
                 .innerHTML += `<option value="${color}">${color}</option>`;
         })
     }
 }
 
-//array of all localstorage keys
-const keys = Object.keys(localStorage);
+class LocalstorageService {
+    static tempSave(product) {
+        localStorage.setItem("tempSave", JSON.stringify(product));
+    }
+
+    static saveToCart(cartProduct) {
+        const keys = Object.keys(localStorage);
+        const id = cartProduct.product.id;
+        const color = cartProduct.color;
+        const qty = cartProduct.qty;
+
+        //return the localstorage key if product is already in cart
+        const productIsAlreadyInCart = LocalstorageService.checkIfProductIsAlreadyInCart(keys, id, color);
+
+        if (!productIsAlreadyInCart) {
+            const key = LocalstorageService.generateKey(keys);
+            localStorage.setItem(key, JSON.stringify(cartProduct));
+        } else {
+            LocalstorageService.addQty(productIsAlreadyInCart, cartProduct);
+        }
+    }
+
+    static checkIfProductIsAlreadyInCart(keys, id, color) {
+        if (localStorage.length > 0) {
+            for (const key of keys) {
+                if ((id === JSON.parse(localStorage.getItem(key)).product.id) && (color === JSON.parse(localStorage.getItem(key)).color)) {
+                    return key;
+                }
+            }
+        }
+        return false;
+    }
+
+    static generateKey(keys) {
+    //new key is the sum of all keys + 1
+        if (localStorage.length > 0) {
+            const sum = (key1, key2) => Number(key1) + Number(key2);
+            return Number(keys.reduce(sum) + 1);
+        }
+        return 0;
+    }
+
+    static addQty(key, cartProduct) {
+        const storedQty = JSON.parse(localStorage.getItem(key)).qty;
+        const newQty = storedQty + cartProduct.qty;
+        const updatedCartProduct = CartProduct.createCartProduct({
+            qty: newQty,
+            color: cartProduct.color,
+            product: cartProduct.product
+        })
+        console.log(updatedCartProduct);
+        localStorage.setItem(key, JSON.stringify(updatedCartProduct));
+    }
+
+    static getTempSave() {
+        const product = JSON.parse(localStorage.getItem("tempSave"));
+        localStorage.removeItem("tempSave");
+        return product;
+    }
+}
 
 fillProductPage();
 
 //save product to localStorage and redirect to cart page
 document.getElementById("addToCart").addEventListener("click", function saveProduct(e) {
     e.preventDefault();
-    saveColor();
-    saveQty();
-    const qty = document.getElementById("quantity");
-    //can only add to cart if quantity is 1 or more and a color is selected
-    if ((Number(qty.value) > 0) && (product.color != "")){
-        const isDuplicate = checkDuplicateProduct(product.name, product.color); 
-        if (!isDuplicate) {
-            const newKey = generateKey(keys);
-            saveNewProduct(newKey);
-        } else {
-            //function addQty(product.qty, isDuplicate);
-            const key = isDuplicate;
-            const storedQty = JSON.parse(localStorage.getItem(key)).qty;
-            product.qty = storedQty + (Number(qty.value));
-            localStorage.setItem(key, JSON.stringify(product));
-            }
-        //redirect to url
-        window.location= "./cart.html";
+    const cartProduct = CartProduct.createCartProduct({
+        qty: getQty(),
+        color: getColor(),
+        product: LocalstorageService.getTempSave()
+    })
+    console.log(cartProduct.color);
+    console.log(cartProduct.qty);
+    if (cartProduct.color != "") {
+    LocalstorageService.saveToCart(cartProduct);
+
+    window.location= "./cart.html";
     }
 })
-
 
 //get product Id from the page Url and save it to productId variable
 function getIdFromUrl() {
@@ -84,54 +149,51 @@ async function getProductFromApi(productId) {
     }
 }
 
-//get product infos and fill product page
+// get product infos and fill product page
 async function fillProductPage() {
     const productId = getIdFromUrl();
-    const product = await getProductFromApi(productId);
-    new Product(product).addProductImgToHtml;
-    new Product(product).addProductNameToHtml;
-    new Product(product).addProductDescriptionToHtml;
-    new Product(product).addProductPriceToHtml;
-    new Product(product).addProductColorsToHtml;
+    const { _id, imageUrl, altTxt, name, description, colors, price } = await getProductFromApi(productId);
+    const productModel = Product.createProduct({
+        id: _id,
+        img: {
+            src: imageUrl,
+            alt: altTxt
+        },
+        name,
+        description,
+        colors,
+        price
+    });
+    ProductHtmlService.addProductImgToHtml({productImgSrc: productModel.img.src, productImgAlt: productModel.img.alt});
+    ProductHtmlService.addProductNameToHtml(productModel.name);
+    ProductHtmlService.addProductDescriptionToHtml(productModel.description);
+    ProductHtmlService.addProductPriceToHtml(productModel.price);
+    ProductHtmlService.addProductColorsToHtml(productModel.colors);
+    LocalstorageService.tempSave(productModel);
+    setMinQtyToOne();
 }
 
-//add productColor to product object
-function saveColor() {
-    const select = document.getElementById("colors");
-    const color = select.options[select.selectedIndex];
-    product.color = color.value;
+function getColor() {
+    const colorsSelect = document.getElementById("colors");
+    const colorOption = colorsSelect.options[colorsSelect.selectedIndex];
+    return colorOption.value;
 }
 
-//add productQty to product object
-function saveQty() {
-    const qty = document.getElementById("quantity");
-    product.qty = Number(qty.value);
+function getQty() {
+    const qtyInput = document.getElementById("quantity");
+    return Number(qtyInput.value);
 }
 
-
-//generate a unique newKey from existing localstorage keys
-function generateKey(keys) {
-    //if localstorage exists, newKey is the sum of all keys + 1 so we can't overwrite an existing key
-    if (localStorage.length > 0) {
-        const reducer = (key1, key2) => Number(key1) + Number(key2);
-        return Number(keys.reduce(reducer) + 1);
+function setMinQtyToOne() {
+    const qtyInput = document.getElementById("quantity");
+    if (qtyInput.value < 1) {
+        qtyInput.value = 1;
     }
-    return 0;
-}
 
-//attribute a newKey to product and save it to localStorage
-function saveNewProduct(newKey) {
-    localStorage.setItem(newKey, JSON.stringify(product));
-}
-
-function checkDuplicateProduct(name, color) {
-    if (localStorage.length > 0) {
-        for (const key of keys) {
-            if ((name === JSON.parse(localStorage.getItem(key)).name) && (color === JSON.parse(localStorage.getItem(key)).color)) {
-                return key;
-            }
+    qtyInput.addEventListener("change", function forceMinQtyToOne(e) {
+        e.preventDefault();
+        if (this.value < 1) {
+            this.value = 1;
         }
-        return false;
-    }
-    return false;
+    })
 }
