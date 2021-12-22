d16c7e97ae480093cbc08ac331857b74347297b4
Romain <hercez92@hotmail.fr>
2021-12-22 12:15:22 +0100

    feat: create a router class

---
+++ front/js/router.js
@@ -0,0 +1,9 @@
+class Router {
+    goToUrl(URL) {
+        window.location= URL;
+    }
+
+    getParamFromUrl(param) {
+        return new URLSearchParams(document.location.search.substring(1)).get(param);
+    }
+}
