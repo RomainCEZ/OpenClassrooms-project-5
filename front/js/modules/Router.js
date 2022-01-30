786edac414ece048eaad599ca09870975b9bef85
Romain <hercez92@hotmail.fr>
2022-01-31 00:58:31 +0100

    refactor: move getParamFromUrl to HtmlService

--- front/js/modules/Router.js
+++ front/js/modules/Router.js
@@ -2,8 +2,4 @@ export default class Router {
     goToUrl(URL) {
         window.location= URL;
     }
-
-    getParamFromUrl(param) {
-        return new URLSearchParams(document.location.search.substring(1)).get(param);
-    }
 }
